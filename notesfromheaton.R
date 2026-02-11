d25a01201d0ba44cd8248c0f79f9b6083fbae0f4
Matthew Wilson <wilson.matthewg@gmail.com>
2026-02-11 14:39:56 -0700

    my work

---
+++ notesfromheaton.R
@@ -0,0 +1,3 @@
+# he used interactiojn model. He validated line addumptions, including avplots
+# answered every question with predictions. hmmmmm
+# set certain constants, predict for that constant and differences in 1 category.
